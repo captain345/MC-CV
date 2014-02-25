d3150c51f9bad5f0b105a4a7e7ea16fb1dcbb6aa
captain345 <thomas@thomass-mbp.uni.mdx.ac.uk>
2014-02-25 18:15:05 +0000

    Code base change

--- forge/mcp/src/minecraft/MCCV/Basic/mccvBase.java
+++ forge/mcp/src/minecraft/MCCV/Basic/mccvBase.java
@@ -1,6 +1,8 @@
 package MCCV.Basic;
 
-import MCCV.Basic.CommonProxy;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -9,6 +11,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
  
 @Mod(modid="MC-CV", name="Minecraft CV Modification", version="0.0.1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
@@ -25,16 +28,19 @@ public class mccvBase {
         @EventHandler
         public void preInit(FMLPreInitializationEvent event) {
                 // Stub Method
-        }
+        }//end of preInit
        
         @EventHandler
         public void load(FMLInitializationEvent event) {
                 proxy.registerRenderers();
-        }
+
+                
+                
+        }//end of load 
        
         @EventHandler
         public void postInit(FMLPostInitializationEvent event) {
                 // Stub Method
-        }
+        }//end of postInit
        
 }
