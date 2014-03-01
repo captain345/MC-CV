fa63701ecd3ae9953283274d4508a0c9d2e0f268
kexD <kexidt@gmail.com>
2014-03-01 17:27:32 +0000

    Added message output to a file in same folder

--- forge/mcp/src/minecraft/MCCV/Basic/ChatEventHook.java
+++ forge/mcp/src/minecraft/MCCV/Basic/ChatEventHook.java
@@ -1,13 +1,32 @@
 package MCCV.Basic;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.ServerChatEvent;
 
 
 
 public class ChatEventHook {
+	PrintWriter output;
+	
+	public ChatEventHook() {
+		
+		try {
+		 output = new PrintWriter(new BufferedWriter(new FileWriter("test.txt")));
+		}
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
 	@ForgeSubscribe
 	public void onServerChatEvent(ServerChatEvent event) {
-		System.out.println("TEST TEST TEST: " + event.message);
+		output.println(event.message);
+		output.flush();
 	}
 }
